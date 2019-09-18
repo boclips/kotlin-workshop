6feeab9912d3788a8906e9267aab20776806586a
José Carlos Valero Sánchez <josecarlos@boclips.com>
2019-09-18 17:35:55 +0100

    Simple Kotlin Introduction
    Signed-off-by: Andrew Bruce <me@andrewbruce.net>

--- out/production/kotlin-workshop/josecarlos/hello.kts
+++ out/production/kotlin-workshop/josecarlos/hello.kts
@@ -1,3 +1,23 @@
 package josecarlos
 
-print("a cool script")
+println("Thanks for joining the talk!")
+ask("Should we get started?")
+printPausing("Great, so kotlin was designed with a few principles in mind!")
+
+
+
+fun ask(vararg question: String) {
+    question.forEach { println(it) }
+    when (readLine()) {
+        "y", "yes" -> return
+        "n", "no" -> ask("There's no other option, I'm afraid, let me ask you again...", question.last())
+        else -> ask("Invalid option please choose [y/n]", question.last())
+    }
+}
+
+fun printPausing(message: String) {
+    println(message)
+    when (readLine()) {
+        else -> return
+    }
+}
